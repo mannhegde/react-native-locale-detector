774e6709d912aedd6d67f3099b0e99e1c962ce71
Manjunath Hegde <manjunathhegde@Manjunaths-MacBook-Pro-2.local>
2025-11-12 23:11:18 +0530

    fixes

--- android/src/main/java/com/i18n/reactnativei18n/ReactNativeI18n.java
+++ android/src/main/java/com/i18n/reactnativei18n/ReactNativeI18n.java
@@ -1,14 +1,12 @@
 package com.i18n.reactnativei18n;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Collections;
 
 
 public class ReactNativeI18n implements ReactPackage {
@@ -21,12 +19,7 @@ public class ReactNativeI18n implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+        return new ArrayList<>();
     }
 }
